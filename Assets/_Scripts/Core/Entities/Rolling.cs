536f2fb2a18fd26d54998f835432ea3f4f2e003d
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2022-08-11 13:27:33 +0300

    Added Rolling script.
    Added Rolling script, but also need to add invulnerability frames. For now rolling script only pushes the samurai

--- Assets/_Scripts/Core/Entities/Rolling.cs
+++ Assets/_Scripts/Core/Entities/Rolling.cs
@@ -2,8 +2,23 @@ using UnityEngine;
 
 namespace Core.Entities
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class Rolling : MonoBehaviour
     {
+        [SerializeField] private Rigidbody2D _rb2d;
+        [SerializeField] private float _force;
 
+        private bool _isRolling = false;
+        public bool IsRolling => _isRolling;
+
+        public void Roll(int direction)
+        {
+            _isRolling = true;
+
+            _rb2d.velocity = Vector2.zero;
+            _rb2d.AddForce(Vector2.right * direction * _force, ForceMode2D.Impulse);
+        }
+
+        public void EndRoll() => _isRolling = false;
     }
 }
